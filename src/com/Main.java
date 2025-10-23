e90454829113ab4be148a9569aea8e6f2813b0f6
Otavio2704 <otavioaredes62@gmail.com>
2025-10-23 20:21:47 -0300

    Update Main.java

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,10 +1,34 @@
 package com.roguelike;
 
 import com.roguelike.game.RoguelikeGame;
+import com.roguelike.ui.UI;
+import com.roguelike.util.InputHandler;
 
 public class Main {
     public static void main(String[] args) {
-        RoguelikeGame game = new RoguelikeGame();
-        game.play();
+        InputHandler inputHandler = new InputHandler();
+        UI ui = new UI(inputHandler);
+
+        boolean running = true;
+        while (running) {
+            ui.showMainMenu();
+            String choice = inputHandler.getCommand();
+
+            switch (choice) {
+                case "1" -> {
+                    ui.showWelcomeMessage();
+                    RoguelikeGame game = new RoguelikeGame();
+                    game.play();
+                }
+                case "2" -> ui.showInstructions();
+                case "3" -> {
+                    System.out.println("\nAté logo! 👋");
+                    running = false;
+                }
+                default -> System.out.println("Opção inválida! Tente novamente.");
+            }
+        }
+
+        inputHandler.close();
     }
 }
