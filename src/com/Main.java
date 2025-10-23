3a7a967071100bff32bca2f5ab8a45c70942cdbc
Otavio2704 <otavioaredes62@gmail.com>
2025-10-23 20:17:54 -0300

    Create Main.java

---
+++ src/com/Main.java
@@ -0,0 +1,10 @@
+package com.roguelike;
+
+import com.roguelike.game.RoguelikeGame;
+
+public class Main {
+    public static void main(String[] args) {
+        RoguelikeGame game = new RoguelikeGame();
+        game.play();
+    }
+}
