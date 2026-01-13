c0b11207f6833763fb7314279614f1477ddd62e8
Otavio2704 <otavioaredes62@gmail.com>
2026-01-13 09:00:53 -0300

    Implement AttackUpgrade power-up class

---
+++ src/com/shop/powerups/AttackUpgrade.java
@@ -0,0 +1,24 @@
+package com.roguelike.shop.powerups;
+
+import com.roguelike.entity.Player;
+import com.roguelike.shop.PowerUp;
+
+public class AttackUpgrade extends PowerUp {
+    private static final String NAME = "Aumento de Ataque";
+    private static final String DESCRIPTION = "Aumenta seu ATK permanentemente em 3 pontos";
+    private static final int PRICE = 75;
+    private static final char SYMBOL = '<|>';
+    private static final int ATK_BONUS = 3;
+
+    public AttackUpgrade() {
+        super(NAME, DESCRIPTION, PRICE, SYMBOL);
+    }
+
+    @Override
+    public void apply(Player player) {
+        int oldAtk = player.getAtk();
+        player.setAtk(player.getAtk() + ATK_BONUS);
+        
+        System.out.println("\n✦ Ataque aumentado de " + oldAtk + " para " + player.getAtk() + "!");
+    }
+}
