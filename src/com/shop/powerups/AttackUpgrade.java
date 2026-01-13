9b3abff44730a99dc08688b757ed90c6e7b3fd9b
Otavio2704 <otavioaredes62@gmail.com>
2026-01-13 09:23:00 -0300

    Update AttackUpgrade.java

--- src/com/shop/powerups/AttackUpgrade.java
+++ src/com/shop/powerups/AttackUpgrade.java
@@ -7,7 +7,7 @@ public class AttackUpgrade extends PowerUp {
     private static final String NAME = "Aumento de Ataque";
     private static final String DESCRIPTION = "Aumenta seu ATK permanentemente em 3 pontos";
     private static final int PRICE = 75;
-    private static final char SYMBOL = '<|>';
+    private static final char SYMBOL = '|';
     private static final int ATK_BONUS = 3;
 
     public AttackUpgrade() {
